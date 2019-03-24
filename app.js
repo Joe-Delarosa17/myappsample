177a8cb4611215dafc59112146a5d2b147720071
Joseph De La Rosa <joseph.delarosa.171@gmail.com>
2019-03-23 23:06:38 -0700

    turned app js into a higher order function cheat sheet   Changes to be committed:

--- app.js
+++ app.js
@@ -1,1 +1,116 @@
-console.log("hello");
+
+const companies= [
+  {name:"company one", category:"finance", start: 1980, end: 2005},
+  {name:"company two", category:"retail", start: 1942, end: 2010},
+  {name:"company three", category:"auto", start: 2000, end: 2011},
+  {name:"company four", category:"finance", start: 2003, end: 2013},
+  {name:"company five", category:"technology", start: 1997, end: 2009},
+  {name:"company six", category:"retail", start: 1985, end: 2002},
+  {name:"company seven", category:"retail", start: 1954, end: 2018},
+  {name:"company eight", category:"food service", start: 1999, end: 2015},
+  {name:"company nine", category:"technology", start: 2001, end: 2010},
+];
+
+const ages = [33, 12, 20, 16, 21, 5, 54, 21, 44, 61, 13, 15, 45, 64, 32];
+
+//for(let i = 0;i<companies.length;i++){
+//  console.log(companies[i]);
+//}
+
+// foreach
+
+//companies.forEach(function(company){
+//  console.log(company.end, company.start);
+//});
+
+// filter
+
+// Get 21 and older from ages array
+//let canDrink = [];
+//for(let i=0;i<ages.length;i++){
+//  if(ages[i] >= 21){
+//    canDrink.push(ages[i]);
+//  }
+//}
+
+//const canDrink = ages.filter(function(age){
+//    if(age >= 21){
+//      return true;
+//    }
+//});
+
+//const canDrink = ages.filter(age => age >= 21);
+
+//Filter retail companies
+
+//const retailCompanies = companies.filter(function(companies){
+//  if(companies.start === 1999){
+//    return true;
+//
+//  }
+//});
+                                        //parameter here
+                                        //if more than parameter
+                                        //use parantheses below
+                                        //before arrow function
+//const retailCompanies = companies.filter(companies => companies.start >= 1980);
+                                    //here ^^^^^^^^
+//console.log(retailCompanies);
+
+// map
+//create array of companies names
+
+//const companyNames = companies.map(function(company){
+//      return company.name;
+//});
+
+//const testMap = companies.map(company => `${company.name} [${company.start} - ${company.end}]`);
+
+
+//console.log(testMap);
+
+
+// sort
+
+
+
+// reduce
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
